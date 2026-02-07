58f5e751cfc5ac3cfdfe9c1154a2a9a5e1dafebb
Petr Tripolsky <tripolskypetr@gmail.com>
2026-02-07 13:15:15 +0500

    types

--- types.d.ts
+++ types.d.ts
@@ -5,6 +5,7 @@ interface IScopedClassRun<Args extends any[]> {
     hasContext(): boolean;
     runInContext<Result = unknown>(callback: () => Result, ...args: Args): Result;
     runOutOfContext<Result = unknown>(callback: () => Result): () => Result;
+    runWithContext<Result = unknown>(callback: () => Result, ...args: Args): () => Result;
     runAsyncIterator<T, TReturn = any, TNext = unknown>(iterator: AsyncGenerator<T, TReturn, TNext>, ...ctorArgs: Args): AsyncGenerator<T, TReturn, TNext>;
     runIterator<T, TReturn = any, TNext = unknown>(generator: Generator<T, TReturn, TNext>, ...ctorArgs: Args): Generator<T, TReturn, TNext>;
 }
